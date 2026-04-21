7bdd6c3eb402022a1410af8d1c5b097173bed0ce
Sean O'Dowd <sp.odowd@outlook.com>
2026-04-20 23:49:13 -0400

    Add classify function to categorize file types

--- src/classifier.py
+++ src/classifier.py
@@ -1,1 +1,18 @@
+#src/classifier.py
 
+def classify(file_path):
+  suffix = file_path.split(".")[-1].lower()
+
+  if suffix in ["jpg", "png", "jpeg"]:
+      return "Images"
+   
+  if suffix in ["pdf", "docx", "txt"]:
+      return "Documents"
+
+  if suffix in ["py", "js", "html"]:
+      return "Code"
+
+  if suffix in ["zip", "rar"]:
+      return "Archives"
+
+  return "Other"
